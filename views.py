878d9bdeee647d4b3eb1d1468689abd3458b75ef
IskandarNabiev <48825508+IskandarNabiev@users.noreply.github.com>
2019-09-29 15:48:03 +0300

    New files

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': ['Если у вас остались вопросы, то задавайте их по телефону','000-000-00',
+                                                             'example@mail.ru']})
+
+
